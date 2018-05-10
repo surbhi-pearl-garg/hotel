84edd6e82f91ca1000d7f9c99ef807ec9301a80f
unknown <surbhi.pearl.garg@Gmail.com>
2018-05-10 17:10:13 +0530

    contact form smtp problem

---
+++ app/Http/Controllers/mailcontroller.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Mail;
+
+class mailcontroller extends Controller
+{
+    public function index()
+    {
+    	return view('index');
+    }
+    public function post(Request $req)
+    {
+    	$req->validate([
+    		'email'=>'required',
+    		'subject'=>'required',
+    		'message'=>'required'
+    	]);
+    	$data=[
+    		'email'=>$req->email,
+    		'subject'=>$req->subject,
+    		'bodyMessage'=>$req->message
+    	];
+    	Mail::send('mail.mail',$data,function($message)use($data){
+    		$message->from('ronantaneja@gmail.com','Surbhi');
+    		$message->to($data['email']);
+    		$message->subject($data['subject']);
+
+    	});
+    	return redirect()->back();
+    }
+}
---
+++ resources/views/mail/mail.blade.php
@@ -0,0 +1,3 @@
+<h3>this is your message</h3>
+<p>{{$bodyMessage}}</p>
+hi hello welcome
